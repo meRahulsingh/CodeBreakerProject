b231c55692eb4214d2244d413d13b5ec6483e2ca
meRahulSingh <mr.rahulsingh@hotmail.com>
2017-11-21 11:08:39 +0100

    Added first changes

--- src/assets/main.js
+++ src/assets/main.js
@@ -6,4 +6,10 @@ function guess() {
     //add functionality to guess function here
 }
 
+function setHIddenFields() {
+    answer = Math.floor(Math.random()*10000);
+    while(answer)
+}
+
+
 //implement new functions here
